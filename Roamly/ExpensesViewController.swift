42cf141493723d2672f9e1c434faa7723a7b25ef
Justin Mai <bmai0002@student.monash.edu>
2024-06-08 17:13:29 +1000

    creating tabbar view controllers

--- Roamly/ExpensesViewController.swift
+++ Roamly/ExpensesViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
-class ExpensesViewController: UIViewController {
+class ExpensesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
 
+    @IBOutlet weak var expenseSearchBar: UISearchBar!
+    @IBOutlet weak var expenseTableView: UITableView!
     @IBOutlet weak var addExpenseViewControllerButton: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        expenseTableView.delegate = self
+        expenseTableView.dataSource = self
+        expenseSearchBar.delegate = self
 
-        // Do any additional setup after loading the view.
+        // Optional: Set up the button action programmatically
+        addExpenseViewControllerButton.target = self
+        addExpenseViewControllerButton.action = #selector(addExpenseTapped)
+    }
+    
+    @objc func addExpenseTapped() {
+        performSegue(withIdentifier: "showAddExpense", sender: self)
+    }
+    
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showAddExpense" {
+            if let addExpenseVC = segue.destination as? AddExpenseViewController {
+                // Pass any necessary data to AddExpenseViewController
+            }
+        }
+    }
+
+    // MARK: - UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of rows for the table
+        return 10 // Replace with your data source count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Configure and return the cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ExpenseCell", for: indexPath)
+        cell.textLabel?.text = "Expense \(indexPath.row + 1)" // Replace with your data
+        return cell
+    }
+
+    // MARK: - UISearchBarDelegate
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        // Handle search bar text changes
     }
     
 
